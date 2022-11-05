4d12ab9046b9e548e95f60f12ed1e919b49c7206
nothingct <nothingct@naver.com>
2022-11-06 08:38:26 +0900

    vue3-compositiono-API

---
+++ src/App.Composition.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 @click="increase">
+    {{ count }} / {{ doubleCount }}
+  </h1>
+  <h1>
+    {{ message }} / {{ reversedMessage }}
+  </h1>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+export default {
+  setup(){
+    const message = ref('Here Comes The Life')
+    const reversedMessage = computed(()=>message.value.split('').reverse().join(''))
+    
+    const count =ref(0)
+    const doubleCount = computed(()=>count.value*2)
+    function increase(){
+      count.value+=1
+    }
+
+    return {
+      message,
+      reversedMessage,
+      count,
+      doubleCount,
+      increase
+
+    }
+  }  
+}
+</script>
